6863832f3c7254061d7b656ede8d93a1b2eb2e96
thompchr <thompchr@ee8d18ea-e5ab-11de-93f0-cd01e99e34ab>
2010-04-11 15:57:55 +0000

    Cleaned some stuff up, created the WEB-INF folder.  Added the logic to copy the war on startup

--- WebText/src/org/webtext/android/WebText.java
+++ WebText/src/org/webtext/android/WebText.java
@@ -1,4 +1,4 @@
- /**************************************************************************
+/**************************************************************************
  * Copyright 2010 Chris Thompson                                           *
  *                                                                         *
  * Licensed under the Apache License, Version 2.0 (the "License");         *
@@ -16,14 +16,72 @@
 
 package org.webtext.android;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.webapp.WebAppContext;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WebText extends Activity {
-	
+
+	private static final String TAG = "WebText";
+
+	private Server server_;
+	private int port_ = 8080;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+
+		AssetManager am = getAssets();
+		try{
+			File outFile = new File("/sdcard/webtext/webtext.war");
+			if (!outFile.exists()){
+				outFile.mkdirs();
+				outFile.createNewFile();
+				InputStream in = am.open("webtext.war");
+				OutputStream out = new FileOutputStream(outFile);
+				byte[] buffer = new byte[1024];
+				int count = in.read(buffer);
+				while(count != -1){
+					out.write(buffer);
+					count = in.read(buffer);
+				}
+				in.close();
+				out.close();	
+			}
+		}catch (IOException ex){
+			Log.e(TAG, "Could not create web files.");
+			ex.printStackTrace();
+		}
+
+		server_ = new Server(port_);
+		Context webtext = new Context(server_, "/webtext");
+		webtext.addServlet(TextServlet.class, "/*");
+
+		WebAppContext webapp = new WebAppContext("/", "/sdcard/webtext/webtext.war");
+		HandlerList hl = new HandlerList();
+		hl.setHandlers(new Handler[] { webapp } );
 		
+		
+		try {
+			server_.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
