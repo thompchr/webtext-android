b619174d152be267e2371033ac020e8ff5b91ce3
thompchr <thompchr@ee8d18ea-e5ab-11de-93f0-cd01e99e34ab>
2010-04-19 22:17:16 +0000

    some changes to get the cometd library to work

--- WebText/src/org/webtext/android/WebText.java
+++ WebText/src/org/webtext/android/WebText.java
@@ -16,19 +16,55 @@
 
 package org.webtext.android;
 
+import org.webtext.android.services.WebTextService;
+import org.webtext.android.services.bindings.IWebTextService;
+
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 
 public class WebText extends Activity {
 
 	private static final String TAG = "WebText";
 	public static final String CONTENT_RESOLVER_ATTRIBUTE = "org.webtext.android.contentResolver";
+	private IBinder binder_ = null;
+	
+	private ServiceConnection webTextServiceConnection_ = new ServiceConnection(){
+
+		@Override
+		public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+			Log.d(TAG, "WebTextService bound to activity");
+			binder_ = arg1;
+			IWebTextService pipe = IWebTextService.Stub.asInterface(binder_);
+			try {
+				pipe.startServer();
+			} catch (RemoteException e) {
+				
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			binder_ = null;
+		}
+		
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-
-
+		
+		Log.v(TAG, "Starting service");
+		
+		Intent intent = new Intent(this, WebTextService.class);
+		startService(intent);
+		bindService(intent, webTextServiceConnection_, BIND_AUTO_CREATE);
 
 	}
 
